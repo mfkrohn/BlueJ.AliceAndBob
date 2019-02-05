71b24755e4bd8d7c1ea2dffae211bed6dbbc93e0
Michael Krohn <mfkrohn@gmail.com>
2019-02-05 17:50:40 -0500

    Finished the lab

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+      Scanner reader = new Scanner(System.in);
+      System.out.println("What is your name?");
+      String name = reader.nextLine();
+      if(name.equals("Alice")){
+      System.out.println("Hello, " + name + ", nice to meet you!");
+    } else if(name.equals("Bob")){
+      System.out.println("Hello, " + name + ", nice to meet you!");
+    }
+    else{
+        System.out.println("Get out, " + name);
 
     }
+}
 }
